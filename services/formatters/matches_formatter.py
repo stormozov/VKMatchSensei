44e0403d964c5bcf65f8b051a0bdf741b6defcd3
Sergey Tormozov <web@stormozov.ru>
2024-12-27 17:53:29 +0300

    refactor(match): Улучшил получение ID фото мэтча
    Внес дополнительное условие проверки на закрытый профиль мэтча, чтобы предотвратить отправку запроса на получение фото мэтча.

--- services/formatters/matches_formatter.py
+++ services/formatters/matches_formatter.py
@@ -11,21 +11,23 @@ def format_matches(match: dict[str, str | int]) -> dict:
 
     user_formatter = DatabaseUserFormatServices()
     vk_api_service = VKApiService()
-    match_id = match.get("id")
+    match_vk_id = match.get("id")
+    photo_id = 0
 
-    photo_id = (
-        vk_api_service
-        .get_user_photos(match_id, rev=1)
-        .get("items", [{"id": 0}])[0]
-        .get("id", 0)
-    )
+    if match.get("is_closed") is False:
+        photo_id = (
+            vk_api_service
+            .get_user_photos(match_vk_id, rev=1)
+            .get("items", [{"id": 0}])[0]
+            .get("id", 0)
+        )
 
     time.sleep(1)
 
     return {
-        "match_id": match_id,
+        "match_id": match_vk_id,
         "first_name": match.get("first_name", ""),
         "last_name": match.get("last_name", ""),
-        "profile_url": user_formatter.get_user_vk_link(match_id),
+        "profile_url": user_formatter.get_user_vk_link(match_vk_id),
         "photo_id": photo_id if photo_id != 0 else None,
     }
