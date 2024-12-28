6c1c9a421ac012b33cd7d9e90993e0ae5a704f83
Sergey Tormozov <web@stormozov.ru>
2024-12-28 12:45:00 +0300

    refactor(match): Заменил форматирование мэтчей на класс
    Заместо функции создал новый класс "MatchFormatter", разделив функцию форматирования на несколько отдельных методов

--- services/formatters/matches_formatter.py
+++ services/formatters/matches_formatter.py
@@ -6,28 +6,49 @@ from services.formatters.db_user_formatter import DatabaseUserFormatServices
 from services.vk_api.vk_api_service import VKApiService
 
 
-def format_matches(match: dict[str, str | int]) -> dict:
-    """Форматирует список мэтчей для записи в базу данных."""
+class MatchFormatter:
+    """Класс для форматирования мэтчей для записи в базу данных."""
 
-    user_formatter = DatabaseUserFormatServices()
-    vk_api_service = VKApiService()
-    match_vk_id = match.get("id")
-    photo_id = 0
+    def __init__(self, match: dict[str, str | int]):
+        self.match = match
+        self.user_formatter = DatabaseUserFormatServices()
+        self.vk_api_service = VKApiService()
 
-    if match.get("is_closed") is False:
-        photo_id = (
-            vk_api_service
-            .get_user_photos(match_vk_id, rev=1)
-            .get("items", [{"id": 0}])[0]
-            .get("id", 0)
+    def format(self) -> dict:
+        """Форматирует мэтч для записи в базу данных."""
+
+        match_vk_id = self.match.get("id")
+        photo_id = self.get_photo_id_if_open(
+            match_vk_id, self.match.get("is_closed")
         )
 
-    time.sleep(1)
+        return self.create_formatted_match_dict(match_vk_id, photo_id)
 
-    return {
-        "match_id": match_vk_id,
-        "first_name": match.get("first_name", ""),
-        "last_name": match.get("last_name", ""),
-        "profile_url": user_formatter.get_user_vk_link(match_vk_id),
-        "photo_id": photo_id if photo_id != 0 else None,
-    }
+    def get_photo_id_if_open(self, match_vk_id: int, is_closed: bool) \
+        -> int | None:
+        """Получает ID фотографии, если профиль не закрыт."""
+
+        if is_closed:
+            return None
+
+        time.sleep(1) # Пауза в 1 секунду, чтобы избежать ограничений API ВК.
+
+        photo_data = self.vk_api_service.get_user_photos(match_vk_id, rev=1)
+        return photo_data.get("items", [{"id": None}])[0].get("id")
+
+    def create_formatted_match_dict(
+        self, match_vk_id: int, photo_id: int | None
+        ) -> dict:
+        """Создает отформатированный словарь для мэтча."""
+        return {
+            "match_id": match_vk_id,
+            "first_name": self.match.get("first_name", ""),
+            "last_name": self.match.get("last_name", ""),
+            "profile_url": self.user_formatter.get_user_vk_link(match_vk_id),
+            "photo_id": photo_id,
+        }
+
+
+def format_matches(match: dict[str, str | int]) -> dict:
+    """Форматирует список мэтчей для записи в базу данных."""
+    return MatchFormatter(match).format() if match else {}
